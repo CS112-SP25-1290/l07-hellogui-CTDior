c9ea149c0d5f024ad90070f52d614bb98aa70e4e
CTDior <83152946+CTDior@users.noreply.github.com>
2025-04-14 20:14:50 -0700

    all steps done

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,53 +1,96 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
-import javafx.application.Application;  //abstract class used for JavaFX GUI's
-import javafx.stage.Stage;              //class for GUI window
-import javafx.scene.Scene;              //class for specific view in GUI window
-import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
-import javafx.scene.control.Label;      //class for label component
-import javafx.scene.control.Button;     //class for button component
-import javafx.event.EventHandler;       //interface for handling events
-import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
+public class HelloApplication extends Application implements EventHandler<ActionEvent> {
+
     /*** GUI COMPONENTS ***/
-    // TODO: follow step 25 in README.md to create reference variables
-    Label label;
+    Label label;            // bottom-right label (updates with text field)
+    Label topLabel;         // top-left label (used as counter)
+    Button button1;         // updates label with text
+    Button button2;         // counts clicks
+    TextField inputField;   // input field for text
+    int clickCount = 0;
+
     /*** DRIVER main ***/
     public static void main(String[] args) {
         launch(args);
     }
 
-    // TODO: follow steps 2-9 in README.md to create a start method
     @Override
-    public void start(Stage primaryStage) throws Exception {
-
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello GUI: Your Name");
 
         StackPane layout = new StackPane();
-
-
         Scene scene = new Scene(layout, 300, 300);
-
-
         primaryStage.setScene(scene);
-
-
         primaryStage.show();
 
+        // Initialize GUI components
+        label = new Label("Hello GUI World"); // bottom-right
+        topLabel = new Label("Count: 0");     // top-left
 
-        label = new Label();
-        label.setText("Hello GUI World");
+        button1 = new Button("Update Label"); // bottom-left
+        button2 = new Button("Count Clicks"); // bottom-right
 
+        inputField = new TextField(); // center
+        inputField.setPromptText("Enter new text");
 
+        // Register event handlers
+        button1.setOnAction(this);
+        button2.setOnAction(this);
+
+        // Layout manager for positioning
         AnchorPane anchorPane = new AnchorPane();
+
+        // Position everything
         AnchorPane.setBottomAnchor(label, 0.0);
         AnchorPane.setRightAnchor(label, 0.0);
-
         anchorPane.getChildren().add(label);
+
+        AnchorPane.setTopAnchor(topLabel, 10.0);
+        AnchorPane.setLeftAnchor(topLabel, 10.0);
+        anchorPane.getChildren().add(topLabel);
+
+        AnchorPane.setBottomAnchor(button1, 10.0);
+        AnchorPane.setLeftAnchor(button1, 10.0);
+        anchorPane.getChildren().add(button1);
+
+        AnchorPane.setBottomAnchor(button2, 10.0);
+        AnchorPane.setRightAnchor(button2, 10.0);
+        anchorPane.getChildren().add(button2);
+
+        AnchorPane.setTopAnchor(inputField, 130.0);
+        AnchorPane.setLeftAnchor(inputField, 50.0);
+        AnchorPane.setRightAnchor(inputField, 50.0);
+        anchorPane.getChildren().add(inputField);
+
+        // Add the anchorPane to the stack layout
         layout.getChildren().add(anchorPane);
     }
-    // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
 
-    // TODO: follow steps 22-34 in README.md to create an event handler
+    /*** Event Handler Logic ***/
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == button1) {
+            // Set label to text field input
+            String input = inputField.getText();
+            if (!input.isEmpty()) {
+                label.setText(input);
+            }
+        } else if (actionEvent.getSource() == button2) {
+            // Increment counter on top label
+            clickCount++;
+            topLabel.setText("Count: " + clickCount);
+        }
+    }
 }
